0a3eeb7aaac5b6a6717967d6b4c238bab430e3e0
alexandetlz <117372173+alexandetlz@users.noreply.github.com>
2023-12-11 17:06:43 +0700

    customer done

--- Project/coba.php
+++ Project/coba.php
@@ -11,11 +11,6 @@ while (($line = fgetcsv($file)) !== FALSE) {
   $sql = "INSERT INTO `customers` (`customerID`, `companyName`, `contactName`, `contactTitle`, `address`, `city`, `region`, `postalCode`, `country`, `phone`, `fax`) 
       VALUES ('".$line[0]."', '".$line[1]."', '".$line[2]."', '".$line[3]."', '".$line[4]."', '".$line[5]."', '".$line[6]."', '".$line[7]."', '".$line[8]."' , '".$line[9]."' , '".$line[10]."')";
       $query = mysqli_query($conn, $sql);
-  // $sql = "INSERT INTO `customers` (`address`) 
-  //     VALUES ('".$line[5]."')";
-  //     $query = mysqli_query($conn, $sql);
-  //     echo $line[5];
-  // print_r($line[0]);
 }
 fclose($file);
 ?>
