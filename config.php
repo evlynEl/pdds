febbd34b2eee07d1d6d1a05007a432ebf76cb3cc
evlynEl <evelyneleonora38@gmail.com>
2023-12-12 11:21:20 +0700

    .

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$dbHost     = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName     = "pdds";
+
+$conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+
+// Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
